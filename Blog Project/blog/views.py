d39116ae38976ec78ce7fca0e394fbcb449d1f63
shims80 <shims80@gmail.com>
2021-05-30 09:43:58 +0300

    update

--- Blog Project/blog/views.py
+++ Blog Project/blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author' :'shimon shitrit',
-        'title': 'Blog Post 1', 
-        'content' : 'first post content',
-        'date_posted' : 'Jun 5 ,1980',
-    },
-    {
-        'author' :'leo my dog',
-        'title': 'Blog Post ', 
-        'content' : 'second post content',
-        'date_posted' : 'March 1 ,2012',
-    }
-]
-# Create your views here.
 
 def home(request):
     context = {
-        'posts':posts
+        'posts':Post.objects.all
     }
     return render (request,'blog/home.html',context)
 
