9baaf6b1c6fbdc08d5a709a910eef247cfd6fcc7
shims80 <shims80@gmail.com>
2021-06-01 13:29:17 +0300

    Part 7 - Login and Logout System

--- Blog Project/blog/urls.py
+++ Blog Project/blog/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from . import views 
-from . import views 
 
 urlpatterns = [
     path('', views.home,name='blog-home'),
     path('about/', views.about,name='blog-about'),
-    path('register/', views.about,name='blog-register'),
 ]
--- Blog Project/blog/views.py
+++ Blog Project/blog/views.py
@@ -4,12 +4,10 @@ from .models import Post
 
 def home(request):
     context = {
-        'posts':Post.objects.all
+        'posts': Post.objects.all()
     }
-    return render (request,'blog/home.html',context)
+    return render(request, 'blog/home.html', context)
+
 
 def about(request):
-    return render (request,'blog/about.html',{'title':'About'})
-
-def register(request):
-    return render (request,'blog/register.html',{'title':'Register'})
+    return render(request, 'blog/about.html', {'title': 'About'})
